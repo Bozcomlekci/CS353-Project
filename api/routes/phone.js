ff29f929e3c3340a120982265894dc5994369be9
YusufZiya Ozgul <yzozgul98@gmail.com>
2021-05-09 12:39:34 +0300

    phone.js

--- api/routes/phone.js
+++ api/routes/phone.js
@@ -5,10 +5,58 @@ const getPool = require('../db');
 
 
 getCustomerPhone = (request, response) => {
-
+    console.log( request.body );
+    username = request.body.username;
+  
+    let pool = getPool();
+    pool.connect((err, client, release) => {
+      if (err) {
+        return console.error('Error acquiring client', err.stack)
+      }
+      client.query('SELECT * FROM Contact where username = $1', [username], (err, result) => {
+        release()
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        }
+  
+        /*if (result.rows.length == 1) {
+          sess.user = {username: username}
+          sess.loggedIn = true;
+        }
+        else {
+          sess.loggedIn = false;
+        }*/
+        console.log( result);
+      })
+    });
 }
 
 getRestaurantPhone = (request, response) => {
+    console.log( request.body );
+    restaurant_id = request.body.restaurant_id;
+  
+    let pool = getPool();
+    pool.connect((err, client, release) => {
+      if (err) {
+        return console.error('Error acquiring client', err.stack)
+      }
+      client.query('SELECT * FROM Contact where restaurant-id = $1', [restaurant_id], (err, result) => {
+        release()
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        }
+  
+        /*if (result.rows.length == 1) {
+          sess.user = {username: username}
+          sess.loggedIn = true;
+        }
+        else {
+          sess.loggedIn = false;
+        }*/
+        console.log(result);
+        response.send(sess);
+      })
+    });
 
 }
 

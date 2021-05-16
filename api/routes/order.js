8ae024418336ec30222024f91c52aa8ba44d3b4f
DESKTOP-414E9N8\Goktan Gudukbay <bupsgoktangudukbay@gmail.com>
2021-05-16 19:21:48 +0300

    Order

--- api/routes/order.js
+++ api/routes/order.js
@@ -11,11 +11,66 @@ createOrder = (request, response) => {
 }
 
 getCustomerOrders = (request, response) => {
+    sess = request.session;
+    if(sess.loggedIn){
+        username = sess.user.username;
+        let pool = getPool();
+        pool.connect((err, client, release) => {
+            if (err) {
+                return console.error('Error acquiring client', err.stack)
+            }
 
+            client.query("SELECT * FROM Orders NATURAL JOIN CompleteOrder"
+            + "NATURAL JOIN Restaurant NATURAL JOIN ConsistOf NATURAL JOIN Address" 
+            + "NATURAL JOIN Orderable where username = $1",
+            [username], (err1, result1) => {
+            if(err1){
+                console.log(err1);
+                response.status(401).send("List Order Unsuccessful");
+            }
+            else{
+                client.query( "SELECT order_id, sum(price) FROM Orders NATURAL JOIN CompleteOrder NATURAL JOIN ConsistOf NATURAL JOIN Orderable" 
+                + "where username = $1 group by(order_id)", [username], (err2, result2) => {
+                    if(err2){
+                        console.log(err2);
+                        response.status(401).send("List Order Unsuccessful");
+                    }
+                    else{
+                        toBeReturned = (result1.rows).concat(result2.rows);
+                        response.status(200).json(toBeReturned); 
+                    }
+                })       
+            }
+        })
+        })
+    }  
+}
+
+getDetailsOfAnOrder = (request, response) => {
+    sess = request.session;
+    if(sess.loggedIn){
+        username = sess.user.username;
+        order_id = request.query.order_id
+        let pool = getPool();
+        pool.connect((err, client, release) => {
+            if (err) {
+                return console.error('Error acquiring client', err.stack)
+            }          
+            client.query( "SELECT * FROM Orders NATURAL JOIN Orderable NATURAL JOIN ConsistOf" 
+            + "NATURAL JOIN Contain CompleteOrder NATURAL JOIN Restaurant NATURAL JOIN"
+            + "Address NATURAL JOIN Item NATURAL JOIN Option_ NATURAL JOIN"
+            + "HasOption NATURAL JOIN DeliveredBy WHERE username = $1 and order_id = $2", [username, order_id], (err1, result1) => {
+                if(err1){
+                    return console.error('Error acquiring client', err.stack)
+                }
+                response.status(200).json(result1); 
+            })
+        })
+    }
 }
 
 getRestaurantOrders = (request, response) => {
-
+    
 }
 
 getDeliveryPersonOrders = (request, response) => {
